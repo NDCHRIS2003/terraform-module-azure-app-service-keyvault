aaad84eccd16aac6c91ca29ea8877320973b8daa
ndchris <ndubuisi.okpala@xerris.com>
2023-05-24 23:56:58 -0600

    Add github actions ci/cd yaml file

--- provider.tf
+++ provider.tf
@@ -6,15 +6,11 @@ terraform {
     }
   }
 
-  backend "azurerm" {
-        # use_msi = true
-        storage_account_name = "dotnetkeyvaultmanagement"
-        container_name       = "tfstatedev"
-        key                  = "vis.terraform.tfstate"
-        resource_group_name = "myAzureKeyVault"
-        # subscription_id      = "6fb71c3b-6ae2-4dcf-988d-5f2373f73bb0"
-        # tenant_id            = "130d9288-fc95-48d4-abff-2cf11f91951f"
-        # access_key           = "nikv3Cmsq3q2cNsElrKQ9fi8SPJlb0JEhWeuFMVe6lbwpnngMr+OXg4bqH3tKQiZIXtuNv31aG+ZsHON3t1E8w=="
+  backend "azurerm" {        
+        # storage_account_name = "dotnetkeyvaultmanagement"
+        # container_name       = "tfstatedev"
+        # key                  = "vis.terraform.tfstate"
+        # resource_group_name = "myAzureKeyVault"        
   }
 }
 
---
+++ terraform-exec/terraform_exec.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -o nounset
+set -o errexit
+ 
+echo "###############################"
+echo "## Starting Terraform script ##"
+echo "###############################"
+
+
+COMMAND=${TERRAFORM_COMMAND}
+APPLY="apply"
+DESTROY="destroy"
+
+cd check_prometheus
+
+terraform init \
+-backend-config="storage_account_name=dotnetkeyvaultmanagement" \
+-backend-config="container_name=tfstatedev" \
+-backend-config="key=vis.terraform.tfstate" \
+-backend-config="resource_group_name=myAzureKeyVault"
+
+terraform validate
+
+terraform plan 
+if [ $COMMAND == $DESTROY ]; then
+    echo "###############################"
+    echo "## Executing terraform destroy ##"
+    echo "###############################"
+    terraform destroy --auto-approve 
+fi
+ 
+if [ $COMMAND == $APPLY ]; then
+    echo "###############################"
+    echo "## Executing terraform apply ##"
+    echo "###############################"
+    terraform apply --auto-approve 
+fi
